b8fe0d12956c2856bbe64abf5cbdd90fc07cf088
Ayush Agrawal <111371722+ayush360@users.noreply.github.com>
2024-05-11 00:10:18 +0530

    component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Logo from "./Logo";
+import Container from './Container/Container';
+import LogoutBtn from "./Header/LogOutBtn";
 
 
-export {Header, Footer}
+export {Header, Footer, Logo, Container, LogoutBtn}
