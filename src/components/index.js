3830135b2b8b5145687add8157a65188e48101f6
Ayush Agrawal <111371722+ayush360@users.noreply.github.com>
2024-07-13 23:53:58 +0530

    Components and Pages

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,13 @@ import LogoutBtn from "./Header/LogoutBtn";
 import PostCard from "./PostCard";
 import Select from "./Select";
 import Input from "./Input";
+import Login from './Login'
+import Signup from './Signup'
+import Button from './Button'
+import AuthLayout from './AuthLayout'
+import PostForm from "./post-form/PostForm";
+import RTE from './RTE'
 
-
-export {Header, Footer, Logo, Container, LogoutBtn, PostCard, Input, Select}
+export {Header, Footer, Logo, Container, LogoutBtn, 
+    PostCard, Input, Select, Login, Signup, Button, 
+    AuthLayout, PostForm, RTE}
